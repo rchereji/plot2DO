61c37019b92d162a32cf68bb6da1e84efa49cb8a
Razvan V. Chereji <r.chereji@gmail.com>
2019-02-06 11:34:58 -0500

    Rename the main file to keep the original name

---
+++ plot2DO.R
@@ -0,0 +1,45 @@
+#!/usr/bin/env Rscript
+
+# v1.0.1 - add Arabidopsis thaliana (TAIR10)
+#        - change option --organism to --genome
+# v1.0.2 - add S. pombe (EF2)
+# v1.0.3 - add chr Y and rDNA for dm6 genome
+# v1.0.4 - correct RangesList issue (see https://support.bioconductor.org/p/109079/)
+# v1.0.5 - refactor
+
+# load app config values:
+library(yaml)
+config <- yaml.load_file("config/config.yaml")
+
+sourceBasePath <- config$application$paths$source
+if(is.null(sourceBasePath)) {
+  sourceBasePath <- file.path(getwd(), "source")
+} 
+
+readsBasePath <- config$application$paths$reads
+if(is.null(readsBasePath)) {
+  readsBasePath <- file.path(getwd(), "data")
+}
+
+testOutputBasePath <- config$application$paths$outTests
+if(is.null(testOutputBasePath)) {
+  testOutputBasePath <- file.path(getwd(), "tests")
+}
+
+annotationsBasePath <- config$application$paths$annotations
+if(is.null(annotationsBasePath)) {
+  # annotationsBasePath <- file.path(getwd(), "Annotations")
+  annotationsBasePath <- getwd()
+}
+
+configBasePath <- config$application$paths$config
+if(is.null(configBasePath)) {
+  configBasePath <- file.path(getwd(), "config")
+}
+
+main <- file.path(sourceBasePath, "plot2DO_main.R")  
+
+source(main)
+
+Main()
+
