7f285788486c9da1922939fb84e7bef34a44807f
Razvan Chereji <r.chereji@gmail.com>
2019-03-07 23:33:07 -0500

    check the minimum version of R>=3.5.0

--- plot2DO.R
+++ plot2DO.R
@@ -1,5 +1,22 @@
 #!/usr/bin/env Rscript
 
+# Check the minimum R version: R>=3.5.0 (required for Bioconductor 3.7)
+getRversion <- function() {
+  rvs <- sub("R version ", "", R.version.string)
+  pos <- gregexpr(" \\(", rvs)[[1]][1]
+  rVersion <- substr(rvs, 1, pos-1)
+  releaseDate <- substr(rvs, pos+2, nchar(rvs)-1)
+  result <- list("version" = rVersion, "date" = releaseDate)
+  return(result)
+}
+
+rvs <- getRversion()
+rVersion <- rvs$version
+releaseDate <- rvs$date
+if (rVersion < "3.5") {
+  stop(paste0("Your R version is ", rVersion,", which is a bit too old (release date: ", releaseDate, ").\nTo run plot2DO, please upgrade R to a version >= 3.5.0.\nYou can download the latest version of R from https://www.r-project.org/"))
+}
+
 suppressPackageStartupMessages({
   library(yaml)
 })
